dda1ab168c473b8194a9273af4de6fa7fe93c550
tentacolo <tenobaldi@gmail.com>
2024-04-16 12:34:02 +0800

    simple main file

--- qeflow/main.py
+++ qeflow/main.py
@@ -2,5 +2,12 @@
 qeflow main file for entry scripts
 '''
 
+
+from qeflow.parsers import getArgs
+
+
 def main():
     print('hello from qeflow main script')
+    args = getArgs()
+
+    print(args)
